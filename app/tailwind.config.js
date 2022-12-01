f19cf79242c7c43ce89014d717c5f00617c4b742
Sean W. Lawrence <sean.w.lawrence+programming@gmail.com>
2022-12-01 12:43:47 -0600

    chore(app/tailwind): install and configure tailwindcss
    install tailwind, postcss, classnames and tw-classname for managing classes and styles easier

---
+++ app/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{jsx,tsx}",
+    "./components/**/*.{jsx,tsx}",
+    "./app/**/*.{jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
