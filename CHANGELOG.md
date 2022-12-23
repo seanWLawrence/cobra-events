79364c1c81a65c6d2689401c7420b80d69078f21
Sean W. Lawrence <sean.w.lawrence+programming@gmail.com>
2022-12-23 10:03:15 -0600

    feat(*): add error and catch boundaries at root level

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,7 +3,12 @@
 
 ### Features
 
-* import tailwind, vitest and components from previous next.js version ([1b45ac0](https://github.com/seanWLawrence/cobra-events/commit/1b45ac0bebf2a1e9d2de304929fc01d37e1c70af))
+* add error and catch boundaries at root level ([e39de7a](https://github.com/seanWLawrence/cobra-events/commit/e39de7a8cf0c0cecf7330fb0c9debb5d47581527))
+* import tailwind, vitest and components from previous next.js version ([830bb80](https://github.com/seanWLawrence/cobra-events/commit/830bb8004c6b1469ae7dc8158cc01421c7ffd8b6))
+
+
+
+
 
 
 
--- app/root.tsx
+++ app/root.tsx
@@ -6,7 +6,12 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useCatch,
+  type ErrorBoundaryComponent,
+  type CatchBoundaryComponent,
 } from "@remix-run/react";
+
+import * as components from "~/components/index.component";
 
 import styles from "~/tailwind.css";
 
@@ -20,15 +25,23 @@ export const meta: MetaFunction = () => ({
   viewport: "width=device-width,initial-scale=1",
 });
 
-export default function App() {
+function Document({
+  children,
+  title = `Remix: So great, it's funny!`,
+}: {
+  children: React.ReactNode;
+  title?: string;
+}) {
   return (
     <html lang="en">
       <head>
+        <meta charSet="utf-8" />
+        <title>{title}</title>
         <Meta />
         <Links />
       </head>
       <body>
-        <Outlet />
+        {children}
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
@@ -36,3 +49,63 @@ export default function App() {
     </html>
   );
 }
+
+export default function App() {
+  return (
+    <Document>
+      <Outlet />
+    </Document>
+  );
+}
+
+export const CatchBoundary: CatchBoundaryComponent = () => {
+  const caught = useCatch();
+
+  return (
+    <Document title="Error">
+      <main className="m-10">
+        <components.SpaceBetween direction="vertical" size="l">
+          <components.Heading as="h1" size="xxxl">
+            Error
+          </components.Heading>
+
+          <components.Paragraph>
+            Something unexpected happened. See below for more details.
+          </components.Paragraph>
+
+          <components.Paragraph>
+            Status code: {caught.status}
+          </components.Paragraph>
+
+          <pre className="bg-slate-100 space-y-5 p-10">
+            <code className="text-sm text-rose-900">
+              {JSON.stringify(caught.data, null, 2)}
+            </code>
+          </pre>
+        </components.SpaceBetween>
+      </main>
+    </Document>
+  );
+};
+
+export const ErrorBoundary: ErrorBoundaryComponent = ({ error }) => {
+  return (
+    <Document title="Error">
+      <main className="m-10">
+        <components.SpaceBetween direction="vertical" size="l">
+          <components.Heading as="h1" size="xxxl">
+            Error
+          </components.Heading>
+
+          <components.Paragraph>
+            Something unexpected happened. See below for more details.
+          </components.Paragraph>
+
+          <pre className="bg-slate-100 p-10 space-y-5 ">
+            <code className="text-sm text-rose-900">{error.stack}</code>
+          </pre>
+        </components.SpaceBetween>
+      </main>
+    </Document>
+  );
+};
