29f615f6b6b3ae4f9876087b4ab1a46737c69dd1
Sean W. Lawrence <sean.w.lawrence+programming@gmail.com>
2023-01-16 01:57:47 -0600

    feat: add links to homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Heading from '$lib/components/heading/heading.svelte';
+	import Paragraph from '$lib/components/paragraph/paragraph.svelte';
+	import SpaceBetween from '$lib/components/space-between/space-between.svelte';
+	import Link from '$lib/components/link/link.svelte';
+</script>
+
+<main class="p-10">
+	<SpaceBetween direction="vertical" size="l">
+		<Heading as="h1">Cobra Events</Heading>
+		<Paragraph>
+			This application is under active development on <Link
+				href="https://github.com/seanWlawrence/cobra-events"
+			>
+				this Github repository
+			</Link>.
+		</Paragraph>
+
+		<Paragraph>
+			Visit <Link href="https://docs.cobra.events">docs.cobra.events</Link> to read the documentation.
+		</Paragraph>
+
+		<Paragraph>
+			You can also peek at the <Link href="/design-system">design system for this site</Link> as it's
+			being worked on.
+		</Paragraph>
+	</SpaceBetween>
+</main>
